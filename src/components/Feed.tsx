1e227453db3476f69765a80d21cc43e8cd766d5d
二ノ宮　大翼 <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2022-04-15 09:23:01 +0900

    Created tweet logic

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { auth } from "../firebase";
+import TweetInput from "./TweetInput";
 
 const Feed = () => {
-  return <button onClick={() => auth.signOut()}>Feed</button>;
+  return (
+    <div>
+      <TweetInput />
+      <button onClick={() => auth.signOut()}>Feed</button>
+    </div>
+  );
 };
 
 export default Feed;
